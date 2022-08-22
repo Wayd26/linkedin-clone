c2283350b9741de74a39f65b14e05f36b149ae53
Wayd26 <wayedoun@gmail.com>
2022-08-22 17:12:42 +0100

    fix app.js bug for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
